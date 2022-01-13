24ed6630fb6e7529b412e166c43d0159c8aff48e
Aditya Nath <84228628+MrEvilGamer@users.noreply.github.com>
2022-01-13 23:16:33 +0530

    Update
    :)

--- server/main.lua
+++ server/main.lua
@@ -1,5 +1,4 @@
-QBCore = nil
-TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
+local QBCore = exports['qb-core']:GetCoreObject()
 
 QBCore.Functions.CreateUseableItem("screwdriverset", function(source, item)
     local Player = QBCore.Functions.GetPlayer(source)
